e5f400c3da49d6554c5720a54c684ba69a1032e0
lanreakalanes <lanreakalanes@gmail.com>
2026-08-20 19:31:14 +0100

    update callback postMessage format

--- api/callback.js
+++ api/callback.js
@@ -1,32 +1,41 @@
 export default async function handler(req, res) {
-  const code = req.query.code;
-  const response = await fetch('https://github.com/login/oauth/access_token', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-    },
-    body: JSON.stringify({
-      client_id: process.env.OAUTH_CLIENT_ID,
-      client_secret: process.env.OAUTH_CLIENT_SECRET,
-      code,
-    }),
-  });
+  const { code } = req.query;
 
-  const data = await response.json();
-  const token = data.access_token;
+  try {
+    const response = await fetch("https://github.com/login/oauth/access_token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        client_id: process.env.OAUTH_CLIENT_ID,
+        client_secret: process.env.OAUTH_CLIENT_SECRET,
+        code,
+      }),
+    });
 
-  const html = `
-    <script>
-      const token = "${token}";
-      const provider = "github";
-      if (window.opener) {
-        window.opener.postMessage('authorization:' + provider + ':success:{"token":"' + token + '","provider":"' + provider + '"}', '*');
-        window.close();
-      }
-    </script>
-  `;
+    const data = await response.json();
+    const token = data.access_token;
 
-  res.setHeader('Content-Type', 'text/html');
-  res.send(html);
+    const script = `
+      <script>
+        (function() {
+          function receiveMessage(e) {
+            console.log("receiveMessage", e);
+            window.opener.postMessage(
+              'authorization:github:success:${JSON.stringify({ token, provider: "github" })}',
+              e.origin
+            );
+          }
+          window.addEventListener("message", receiveMessage, false);
+          window.opener.postMessage("authorizing:github", "*");
+        })();
+      </script>
+    `;
+
+    return res.status(200).send(script);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 }
